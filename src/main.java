638204395c829f7c63a2914816ad43c505b7fc5f
Win10Pro <met4ka@gmail.com>
2022-03-31 21:01:00 +0400

    msg2

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
+
 public class main {
-    public static void main (String[] args){
-        System.out.println("hello");
+    public static void main(String [] args) {
+        System.out.println("Hello");
+        for(int x=1; x<=10; x++){
+            System.out.println("X="+x);
+        }
     }
 }
